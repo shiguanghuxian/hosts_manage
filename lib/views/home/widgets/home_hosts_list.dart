219a1ef13420bd16e3334c15090816984be674a4
zuoxiupeng <zuoxiupeng@live.com>
2021-11-12 21:20:56 +0800

    保存一下

--- lib/views/home/widgets/home_hosts_list.dart
+++ lib/views/home/widgets/home_hosts_list.dart
@@ -6,6 +6,7 @@ import 'package:hosts_manage/components/macos_alert_dialog.dart';
 import 'package:hosts_manage/i18n/i18n.dart';
 import 'package:hosts_manage/store/store.dart';
 import 'package:hosts_manage/views/home/bloc/home_bloc.dart';
+import 'package:hosts_manage/views/home/widgets/hosts_add_widget.dart';
 import 'package:hosts_manage/views/home/widgets/hosts_list_widget.dart';
 import 'package:macos_ui/macos_ui.dart';
 
@@ -68,46 +69,7 @@ class _HomeHostsListState extends State<HomeHostsList> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        InkWell(
-                          onTap: () {
-                            showMacOSAlertDialog(
-                              context: context,
-                              builder: (_) => MacOSAlertDialog(
-                                title: Text(
-                                  'Alert Dialog with Primary Action',
-                                  style: MacosTheme.of(context)
-                                      .typography
-                                      .headline,
-                                ),
-                                message: Text(
-                                  'This is an alert dialog with a primary action and no secondary action',
-                                  textAlign: TextAlign.center,
-                                  style: MacosTheme.of(context)
-                                      .typography
-                                      .headline,
-                                ),
-                                primaryButton: PushButton(
-                                  buttonSize: ButtonSize.large,
-                                  child: Text('Primary'),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                                secondaryButton: PushButton(
-                                  buttonSize: ButtonSize.large,
-                                  child: Text('Primary'),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                              ),
-                            );
-                          },
-                          child: MacosIcon(
-                            CupertinoIcons.add_circled,
-                            color: MacosTheme.of(context).primaryColor,
-                          ),
-                        ),
+                        const HostsAddWidget(),
                         InkWell(
                           onTap: () {
                             context
---
+++ lib/views/home/widgets/hosts_add_widget.dart
@@ -0,0 +1,111 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:hosts_manage/components/macos_alert_dialog.dart';
+import 'package:hosts_manage/i18n/i18n.dart';
+import 'package:hosts_manage/store/store.dart';
+import 'package:hosts_manage/views/home/bloc/home_bloc.dart';
+import 'package:macos_ui/macos_ui.dart';
+
+// 添加hosts配置
+class HostsAddWidget extends StatefulWidget {
+  const HostsAddWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _HostsAddWidgetState createState() => _HostsAddWidgetState();
+}
+
+class _HostsAddWidgetState extends State<HostsAddWidget> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  String hostsName = '';
+  TextEditingController hostsNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    hostsNameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreBuilder<ZState>(
+      builder: (context, store) {
+        I18N lang = StoreProvider.of<ZState>(context).state.lang;
+        return BlocBuilder<HomeBloc, HomeState>(
+          buildWhen: (previous, current) {
+            return false;
+          },
+          builder: (context, state) {
+            return InkWell(
+              onTap: () {
+                hostsNameController.text = '';
+                context.read<HomeBloc>().add(const ChangeEditListEvent(false));
+                showMacOSAlertDialog(
+                  context: context,
+                  builder: (BuildContext context) => MacOSAlertDialog(
+                    title: Text(
+                      '添加hosts配置',
+                      style: MacosTheme.of(context).typography.headline,
+                    ),
+                    message: Container(
+                      margin: EdgeInsets.only(bottom: 8),
+                      child: Column(
+                        children: [
+                          Container(
+                            child: MacosTextField(
+                              controller: hostsNameController,
+                              onChanged: (String val) {
+                                setState(() {
+                                  hostsName = val;
+                                });
+                              },
+                              autofocus: true,
+                              placeholder: '请输入hosts配置显示名',
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    primaryButton: PushButton(
+                      color: Colors.grey[350],
+                      buttonSize: ButtonSize.large,
+                      child: Text(lang.get('public.cancel')),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    secondaryButton: PushButton(
+                      color: Colors.green[400],
+                      buttonSize: ButtonSize.large,
+                      child: Text(lang.get('public.confirm')),
+                      onPressed: () {
+                        setState(() {
+                          hostsName = '';
+                        });
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                );
+              },
+              child: MacosIcon(
+                CupertinoIcons.add_circled,
+                color: MacosTheme.of(context).primaryColor,
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
