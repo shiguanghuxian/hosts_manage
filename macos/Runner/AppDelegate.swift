b7b8fb698ad277c4ec3304d90eb485999406d2ba
shiguanghuxian <zuoxiupeng@live.com>
2021-11-13 21:30:30 +0800

    macos关闭按钮不退出应用

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -4,6 +4,18 @@ import FlutterMacOS
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    Swift.print("applicationShouldTerminateAfterLastWindowClosed")
+    NSApp.hide(nil)
+    return false
+  }
+        
+  override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+    Swift.print("applicationShouldHandleReopen")
+    if !flag {
+      for window: AnyObject in NSApplication.shared.windows {
+        window.makeKeyAndOrderFront(self)
+      }
+    }
     return true
   }
 }
